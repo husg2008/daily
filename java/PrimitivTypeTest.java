cb5b34c24a54301dbd596a76070994ce4963f910
husg2008 <husg@radi.ac.cn>
2020-05-28 17:36:03 +0800

    add java

---
+++ java/PrimitivTypeTest.java
@@ -0,0 +1,15 @@
+public class PrimitivTypeTest{
+	static String str;
+	public static void main(String[] args){
+		//byte
+		System.out.println("基本类型byte二进制位数："+ Byte.SIZE);
+		System.out.println();
+		//int
+		System.out.println("int的最小值="+ Integer.MIN_VALUE);
+		//long
+		System.out.println("long的最大值="+ Long.MAX_VALUE);
+		//打印默认值
+		System.out.println("String的默认值："+ str);
+	}
+}
+
