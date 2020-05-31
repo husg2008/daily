fcf0fd2750539e52d4a86493a8554036cb68925b
husg2008 <husg@radi.ac.cn>
2020-05-31 22:33:30 +0800

    531

--- java/Overloading.java
+++ java/Overloading.java
@@ -1,28 +1,31 @@
 //:initialization/Overloading.java
 //demonstration of both constructor
 //and ordinary method overloading.
-import static net.mindview.util.Print.*;
+//import java.util.*;
+//import static net.mindview.util.Print.*;是Java四版中作者自己写的jav包
+
 
 class Tree{
 	int height;
 	Tree(){
-		print("Planting a seedling");
+		System.out.println("Planting a seedling");
 		height = 0;
 	}
 	Tree(int initialHeight){
 		height=initialHeight;
-		print("Creating new Tree that is " + height + "feet tall");
+		System.out.println("Creating new Tree that is " + height + " feet tall");
 	}
 	void info(){
-		print("Tree is " + height + "feet tall");
+		System.out.println("Tree is " + height + " feet tall");
 	}
 	void info(String s){
-		print(s + ": Tree is " + height + "feet tall");
+		System.out.println(s + ": Tree is " + height + " feet tall");
 	}
+}
 
 	public class Overloading{
 		public static void main(String[] args){
-			for(int i=0;i<5;i++){
+			for(int i=0;i<7;i++){
 				Tree t = new Tree(i);
 				t.info();
 				t.info("Overloading method");
