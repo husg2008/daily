dd07e82036d6795cefd35f3c8af48ecb5d3070ff
husg2008 <husg@radi.ac.cn>
2020-07-18 09:31:36 +0800

    home

--- dev/Vueopenlayers/src/utils/constant.js
+++
@@ -1,9 +0,0 @@
-const icontype = [
-  ['1']: {
-    imgSize: [30, 19],
-    src: "./static/img/配气站-30-19.png",
-    scale:1,
-  }
-]
-
-export { icontype }
--- java/Animal.java
+++
@@ -1,5 +0,0 @@
-/* 文件名 : Animal.java */
-interface Animal {
-   public void eat();
-   public void travel();
-}
